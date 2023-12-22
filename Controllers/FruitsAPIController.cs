271f51e2c98954ae8a443e92aafe0ed500582d49
Umar Mr. Wasim <umar.wasim@corp.wizlinx.com>
2023-12-22 19:47:56 +0500

    Initial commit

---
+++ Controllers/FruitsAPIController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FruitsAPIController : ControllerBase
+    {
+        public List<string> Fruits = new List<string>()
+        {
+            "Apple",
+            "Mango",
+            "Banana",
+            "Grapes"
+        };
+        [HttpGet]
+        public List<string> getFruits()
+        {
+            return Fruits;
+        }
+        [HttpGet("{id}")]
+        public string GetFruitsByIndex(int id)
+        {
+            return Fruits.ElementAt(id);
+        }
+    }
+}
